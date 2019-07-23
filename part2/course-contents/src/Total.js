4f05a22838739dfcd2c198da6b80cb2c09550f4f
Steve Phuc <stevephuc.dev@gmail.com>
2019-07-23 16:27:34 +0300

    2.6

--- part2/course-contents/src/Total.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Total = ({ parts }) => <b>Total of {parts.reduce((s, p) => s + p.exercises, 0)} exercises</b>;
-
-export default Total;
---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const App = () => {
+    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [newName, setNewName] = useState("");
+
+    const personToShow = persons.map(person => {
+        return <p key={person.name}>{person.name}</p>;
+    });
+
+    const handleNewName = e => {
+        setNewName(e.target.value);
+    };
+
+    const addPerson = e => {
+        e.preventDefault();
+        setPersons([...persons, { name: newName }]);
+        setNewName("");
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+                <div>
+                    name: <input value={newName} onChange={handleNewName} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>debug: {newName}</div>
+            <div>{personToShow}</div>
+        </div>
+    );
+};
+
+export default App;
